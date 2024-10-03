06b8cc5fd639dc66c9b6b89e44241c6b39afff9a
Vikas Sahni <vikassahni.tech@gmail.com>
2024-10-03 11:14:23 +0530

    first project completed

--- 001-notes-app/script.js
+++ 001-notes-app/script.js
@@ -6,20 +6,28 @@ let noteCount = +dataObjKeys[dataObjKeys.length - 1] || 0;
 
 const createNoteBox = (id) => {
   const div = document.createElement("div");
-  div.classList.add("w-96", "h-96", "border-black", "border-2", "relative");
-  div.innerHTML = `<textarea class="w-full h-full resize-none p-4" placeholder="Write..."></textarea>
-                    <img class="w-8  absolute right-4 bottom-4" src="./images/delete.png" alt="delete-btn">`;
+  div.classList.add(
+    "max-w-sm",
+    "w-full",
+    "h-96",
+    "border-white",
+    "border-2",
+    "relative",
+    "rounded-2xl"
+  );
+  div.innerHTML = `<textarea class="w-full h-full resize-none p-4 rounded-2xl border-none outline-none" placeholder="Write..."></textarea>
+                    <img class="w-8  absolute right-4 bottom-4" src="./images/delete.png" alt="delete-btn">
+                     <span style="display:none" class="bg-[#892CE8] text-white text-[10px] px-2 py-1 text-center rounded-full absolute right-1 bottom-14">deleted</span>                    
+                    `;
   notesContainer.prepend(div);
   div.id = id;
 
   data[div.id] = data[div.id] || "";
   localStorage.setItem("data", JSON.stringify(data));
-  if(data[div.id]){
-    div.querySelector("textarea").value=data[div.id]
-
+  if (data[div.id]) {
+    div.querySelector("textarea").value = data[div.id];
   }
   div.addEventListener("input", (e) => {
-    console.log("vikas");
     data[div.id] = e.target.value;
     localStorage.setItem("data", JSON.stringify(data));
   });
@@ -28,26 +36,19 @@ const createNoteBox = (id) => {
   deleteButton.addEventListener("click", () => {
     delete data[deleteButton.parentElement.id];
     localStorage.setItem("data", JSON.stringify(data));
-
-    deleteButton.parentElement.remove();
+    deleteButton.nextElementSibling.style.display = "block";
+    setTimeout(() => {
+      deleteButton.parentElement.remove();
+    }, 150);
   });
 };
 
-console.log(dataObjKeys);
-
 dataObjKeys.forEach((key) => {
-  // console.log(key);
   createNoteBox(key);
-  // const div = document.createElement("div");
-
-  // div.classList.add("w-96", "h-96", "border-black", "border-2", "relative");
-  // div.innerHTML = `<textarea class="w-full h-full resize-none p-4" name="" id="" placeholder="Write..."></textarea>
-  //                  <img class="w-8  absolute right-4 bottom-4" src="./images/delete.png" alt="">`;
-  //         div.querySelector("textarea").value=data[key]
-
-  // notesContainer.prepend(div);
 });
 
 createNotesButton.addEventListener("click", () => {
   createNoteBox(++noteCount);
 });
+
+
